4c3249fdc4ecf1d1d8843e4a94f46d84cdc41683
jay-amin-tops <jay.amin.tops@gmail.com>
2023-05-23 10:44:58 +0530

    first javascript intro

---
+++ JavaScript/custom.js
@@ -0,0 +1,10 @@
+document.getElementById("addclass").addEventListener("click", function () {
+    console.log("addclass called");
+})
+document.getElementById("removeclass").addEventListener("click", function () {
+    console.log("removeclass called");
+})
+function toggleClassExample() {
+    console.log("toggleClassExample called");
+
+}
