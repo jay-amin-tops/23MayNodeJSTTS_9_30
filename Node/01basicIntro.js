6914b2efa2121a87a20e3ce23737b23d3e11ae25
jay-amin-tops <jay.amin.tops@gmail.com>
2023-06-08 12:48:01 +0530

    data

---
+++ Node/01basicIntro.js
@@ -0,0 +1,212 @@
+// console.log("called");
+
+// var a = 20;
+// var var_name = "Tops"
+// var status = true;
+// console.log(a);
+
+
+// var a = 50;
+// var b = 55;
+// let sum = a+b
+// console.log(sum);
+
+
+// var Emp = {
+//     Name: "Tops",
+//     Email: "jay.amin.tops@gmail.com",
+//     Dept: "Node"
+// }
+
+// console.log(Emp.Name)
+// console.log(Emp.Dept)
+// console.log(Emp.Email)
+
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b)
+// console.log(a % b);
+
+//relational
+// console.log(a > b);
+// console.log(a < b);
+
+//assignment
+n1 = 10;
+
+//n1 = n1 - 2;
+n1 += 2;
+//console.log(n1);
+
+//logical
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// console.log(!true)
+
+//conditional
+var x = 10;
+var y = 20;
+
+// if (x > y) {
+//     console.log("X is greater");
+// }
+// else {
+//     console.log("Y is greater");
+// }
+
+//loping ;
+
+// for (var i = 1; i < 10; i++) {
+//     console.log(i);
+// }
+
+var ar = [10, 20, 30, 40, 50];
+
+// for (var i = 0; i < ar.length; i++) {
+//     console.log(ar[i]);
+// }
+
+// var i = 25;
+// while (i < 25) {
+
+//     i++;
+//     console.log(i);
+// }
+
+// var i = 25;
+// do {
+
+//     i++;
+//     console.log(i);
+// } while (i < 25);
+
+
+// var subject = [ "Php", "Node", "React","Java"];
+
+// console.log("Ori Array",subject)
+// subject.pop();
+// console.log("after Pop()",subject)
+// subject.shift();
+// console.log("after shift()",subject)
+// subject.unshift("Android");
+// console.log("after unshift()",subject)
+// subject.push("Vue");
+// console.log("after push()",subject)
+
+
+
+var str = "sun rISes in east";
+
+// console.log(str.length)
+// console.log(str[0])
+// console.log(str[5])
+// console.log(str.charAt(2))
+// console.log(str.indexOf('u'))
+// console.log(str)
+// console.log(str.trim());
+// console.log(str.toLowerCase().trim())
+// console.log(str.toUpperCase())
+// console.log(str.slice(4, 8))
+// const array1 = [1, 2, 'a', '1a'];
+// // console.log(array1.join());
+// console.log(typeof array1);
+// var arraytostr = array1.join('-')
+// console.log(typeof arraytostr);
+
+
+
+
+var subject = [ "Php", "Node", "React","Java"];
+
+//subject.pop();
+//console.log(subject.length)
+//subject.shift();
+//subject.shift();
+//subject.unshift("Android");
+//subject.push("ios");
+// subject.forEach(function (i) {
+//     console.log(i);
+// })
+//console.log(subject)
+
+var str = "sun rises in east";
+
+// console.log(str.length)
+// console.log(str.charAt(5))
+// console.log(str.indexOf('u'))
+// console.log(str)
+// console.log(str.trim());
+// console.log(str.toLowerCase().trim())
+// console.log(str.toUpperCase())
+// console.log(str.slice(4, 8))
+
+// function myfunc() {
+//     console.log("function calling....")
+// }
+
+// function square(num) {
+//     //console.log("square of " + num + " is " + (num * num));
+//     console.log(`Square of ${num} is ${num * num}`);
+// }
+
+var avg = (a, b) => {
+    console.log(`avg is ${(a + b) / 2}`)
+}
+
+// myfunc();
+// square(10);
+// square(20)
+// avg(10, 20);
+
+
+// class Animal {
+//     eat() {
+//         console.log("animal eating grass")
+//     }
+// }
+// var myDemo = new Animal();
+// myDemo.eat();
+
+
+// class Calc {
+
+//     constructor(val) {
+//         console.log("constructor calling..." + val);
+//     }
+
+//     add(a, b) {
+//         console.log(a + b);
+//     }
+
+//     static display() {
+//         console.log("running display method....")
+//     }
+// }
+
+// var myCalc = new Calc(10);
+// myCalc.add(10, 20);
+
+//Calc.display();
+
+
+// var myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getDate())
+// console.log(myDate.getDay())
+// console.log(myDate.getFullYear())
+// console.log(myDate.getMonth())
+// console.log(myDate.getTime())
+// console.log(myDate.getHours())
+// console.log(myDate.getMinutes())
+
